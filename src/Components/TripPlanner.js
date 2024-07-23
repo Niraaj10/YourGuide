fa1afe325daa56929ef78281bb7bd8611f0b41bc
Niraaj10 <nitcode10@gmail.com>
2024-07-23 20:11:31 +0530

    .

--- src/Components/TripPlanner.js
+++ src/Components/TripPlanner.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { fetchTripData, fetchTripDataC } from '../ApiData/ApiData'
 import fullText from '../ApiData/Dataa';
 import search from '../Assests/svg/Search.svg';
 import BgTP from '../Assests/img/bgTP.jpg';
 import { BounceLoader } from 'react-spinners'
+import Footer from './Footer';
 
 
 
@@ -14,39 +15,35 @@ const TripPlanner = () => {
   const [days, setDays] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const ContRef = useRef(null);
 
 
-  const splitText = (text) => {
-    const parts = text.split('\n\nHotels:\n');
-    const cityDescription = parts[0];
-    setCDes(cityDescription);
-    const hotelParts = parts[1].split('\n\nDay ');
-    const hotelss = hotelParts[0].trim().split(/\n(?=\d+\.\s)/);
-    console.log(hotelss);
+  // const splitText = (text) => {
+  //   const parts = text.split('\n\nHotels:\n');
+  //   const cityDescription = parts[0];
+  //   setCDes(cityDescription);
+  //   const hotelParts = parts[1].split('\n\nDay ');
+  //   const hotelss = hotelParts[0].trim().split(/\n(?=\d+\.\s)/);
+  //   console.log(hotelss);
 
-    setHotels(hotelss);
-    const days = 'Day ' + hotelParts.slice(1).join('\n\nDay ');
-    const perrDayss = days.split('Day').slice(1);
-    setDays(perrDayss);
-    return { cityDescription, hotelss, perrDayss };
-  };
+  //   setHotels(hotelss);
+  //   const days = 'Day ' + hotelParts.slice(1).join('\n\nDay ');
+  //   const perrDayss = days.split('Day').slice(1);
+  //   setDays(perrDayss);
+  //   return { cityDescription, hotelss, perrDayss };
+  // };
 
   const handleClick = async () => {
     setLoading(true)
+
+    if (ContRef.current) {
+      ContRef.current.scrollIntoView({ behaviur: 'smooth' });
+    }
 
     try {
       // const PlanText = await fetchTripData('Pune', '2024-08-01', '2024-08-05');
       const PlanText = await fetchTripDataC('Pune', '2024-08-01', '2024-08-05');
       setLoading(false)
-      // const { cityDescription, hotelss, PerrDayss } = splitText(PlanText);
-      //   // const days = splitText(PlanText) ;
-      //   setCDes(cityDescription);
-      // // console.log(cDes);
-      // setHotels(hotelss);
-      // // console.log(hotels);
-      // setDays(PerrDayss);
-      // // console.log(days);
-
 
       const parts = await PlanText.split('\n\nHotels:\n');
       const cityDescription = parts[0];
@@ -64,14 +61,6 @@ const TripPlanner = () => {
       setLoading(false)
     }
 
-    // const { cityDescription, hotelss, perrDayss } = splitText(fullText);
-
-    // console.log(cDes);
-
-    // console.log(hotels);
-
-    // console.log(days);
-
   }
 
 
@@ -83,32 +72,32 @@ const TripPlanner = () => {
     <>
       <div className='mt-32 relative'>
 
-      <img src={BgTP} alt="" className='absolute top-[-320px] rounded-b-[70px]' />
+        <img src={BgTP} alt="" className='absolute top-[-320px] rounded-b-[70px]' />
 
         <div className='w-full relative z-20  flex flex-col mx-auto justify-center items-center'>
-          <div className='text-white mt-[125px]'>
+          <div className='text-white mt-[160px]'>
             <div className='font-bold text-sm mb-[-42px]'>Plan your trip with us with the help of AI</div>
-            <div className='fig text-[150px] font-bold drop-shadow-3xl' style={{ textShadow: '1px 1px 15px rgba(0, 0, 0, 0.6)' }}>Make your trip</div>  
+            <div className='fig text-[150px] font-bold drop-shadow-3xl' style={{ textShadow: '1px 1px 15px rgba(0, 0, 0, 0.6)' }}>Make your trip</div>
           </div>
           <div className='Inputs '>
             <div></div>
-            <div className='flex flex-col gap-3 bg-white px-20 py-10 border rounded-3xl shadow-xl'>    
+            <div className='flex flex-col gap-3 bg-white px-20 py-10 border rounded-3xl shadow-xl'>
               <div className='text-sm font-bold'>Please enter your trip details</div>
               <div className='border p-2 rounded-xl flex justify-center items-center'>
                 <input type="text" className='outline-none focus:outline-none w-80 pl-5' placeholder='Enter city' />
                 <div className='relative'>
-                <div className='absolute top-[-28px;] right-[41px] text-xs bg-white px-2 font-semibold text-gray-300'>End-date</div>
-                <input type="date" name="" id="" className='outline-none focus:outline-none border-l px-2'placeholder='Enter city'  />
+                  <div className='absolute top-[-28px;] right-[41px] text-xs bg-white px-2 font-semibold text-gray-300'>End-date</div>
+                  <input type="date" name="" id="" className='outline-none focus:outline-none border-l px-2' placeholder='Enter city' />
                 </div>
 
                 <div className='relative'>
-                <div className='absolute top-[-28px;] right-[63px] text-xs bg-white px-2 font-semibold text-gray-300'>End-date</div>
-                <input type="date" name="" id="" className='outline-none focus:outline-none border-l px-2 border-r mr-5' placeholder='Enter city' />
+                  <div className='absolute top-[-28px;] right-[63px] text-xs bg-white px-2 font-semibold text-gray-300'>End-date</div>
+                  <input type="date" name="" id="" className='outline-none focus:outline-none border-l px-2 border-r mr-5' placeholder='Enter city' />
                 </div>
                 <button onClick={handleClick} className='bg-[#41D6C7] p-3 rounded-full'>
-                    <img src={search} alt="" className=''/>
-                  </button>
-              </div>    
+                  <img src={search} alt="" className='' />
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -117,46 +106,63 @@ const TripPlanner = () => {
           <button onClick={handleClick} className='bg-black text-white p-4 mx-auto'>Click</button>
         </div> */}
 
-        <div>
+        <div
+          ref={ContRef}
+          className={`SearchCont bg-white px-36 py-2 pt-24 relative
+        ${loading === false ? 'h-fit bg-[#FCFCFD]' : 'h-[40vh] '} `}
+        >
 
-        {loading && <>
-          <div className='mx-auto absolute top-28 left-[50%]'>
-          <BounceLoader color="#41D6C7" />
-          </div>
+          {loading && <>
+            <div className='mx-auto absolute top-28 left-[50%]'>
+              <BounceLoader color="#41D6C7" />
+            </div>
           </>}
 
-          {error && 
-          <div className='mx-auto absolute top-28 left-[50%]'>
-          Sorryyy, No information found please enter valid place...
-          </div>
+
+          {error &&
+            <div className='mx-auto absolute top-28 left-[50%]'>
+              Sorryyy, No information found please enter valid place...
+            </div>
           }
 
-          <button onClick={handleClick}>Fetch Trip Data</button>
-          <div className='mb-7'>
-            <h2 className='p-5'>City Description</h2>
-            <p>{cDes}</p>
+          <div
+          className={` ${cDes === '' ? 'invisible' : 'visible'} `}
+          >
+
+            <div>
+                <button onClick={handleClick}>Fetch Trip Data</button>
+                <div className='mb-7'>
+                  <h2 className='p-5'>City Description</h2>
+                  <p>{cDes}</p>
+                </div>
+                <div className='mb-7'>
+                  <h2>Hotels</h2>
+                  <p>{hotels.map((hot, index) => (
+                    <li key={index}>{hot}</li>
+                  ))}</p>
+                </div>
+                <div className='mb-7'>
+                  <h2>Days</h2>
+                  <ul>
+                    {days.map((day, index) => (
+                      <li key={index}>{day}</li>
+                    ))}
+                  </ul>
+                </div>
+            </div>
           </div>
-          <div className='mb-7'>
-            <h2>Hotels</h2>
-            <p>{hotels.map((hot, index) => (
-              <li key={index}>{hot}</li>
-            ))}</p>
-          </div>
-          <div className='mb-7'>
-            <h2>Days</h2>
-            <ul>
-              {days.map((day, index) => (
-                <li key={index}>{day}</li>
-              ))}
-            </ul>
-          </div>
+          
+
+
+
+
         </div>
 
 
-
+        <Footer />
       </div>
 
-    
+
     </>
   )
 }
