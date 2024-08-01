80e0b6e60017242c69ab061c49e63bd541356f1b
Niraaj10 <nitcode10@gmail.com>
2024-08-01 22:08:46 +0530

    .

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Bg from '../Assests/img/LoginBg.jpg'
 
 const Login = () => {
   return (
     <>
+      <div>
       <div className='m-auto h-[100vh] w-full flex justify-center items-center'>
+      <img src={Bg} alt="" className='absolute z-0' />
+      <div className='relative z-10'>
+        YourGuide
+      </div>
+
+      <div className=' mt-9 relative z-10 h-[74vh] w-[37vw] border bg-white rounded-3xl'>
+
+
         Login
+      </div>
+
+      </div>
+      
       </div>
     </>
   )
