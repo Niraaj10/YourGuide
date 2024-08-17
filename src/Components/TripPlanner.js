faca0fbe0bc3f6a634a08c7a32e37b42e6d5c8b8
Niraaj10 <nitcode10@gmail.com>
2024-08-17 18:52:01 +0530

    .

--- src/Components/TripPlanner.js
+++ src/Components/TripPlanner.js
@@ -5,6 +5,7 @@ import search from '../Assests/svg/Search.svg';
 import down from '../Assests/svg/down.svg';
 import MMap from '../Assests/svg/MagicMap.svg'
 import BgTP from '../Assests/img/bgTP.jpg';
+import BgTP2 from '../Assests/img/Tp2.jpg';
 import { BounceLoader } from 'react-spinners'
 import Footer from './Footer';
 import Mapp from './Mapp';
@@ -125,31 +126,32 @@ const TripPlanner = () => {
 
   return (
     <>
-      <div className='mt-32 relative'>
+      <div className='mt-0 lg:mt-32 relative'>
 
-        <img src={BgTP} alt="" className='absolute top-[-320px] rounded-b-[70px]' />
+        <img src={BgTP} alt="" className='absolute top-[-320px] rounded-b-[70px] hidden lg:block' />
+        <img src={BgTP2} alt="" className='object-contain absolute top-[-93px] rounded-b-[50px]  lg:hidden' />
 
         <div className='w-full relative z-20  flex flex-col mx-auto justify-center items-center'>
-          <div className='text-white mt-[160px]'>
-            <div className='font-bold text-sm mb-[-42px]'>Plan your trip with us with the help of AI</div>
-            <div className='fig text-[150px] font-bold drop-shadow-3xl' style={{ textShadow: '1px 1px 15px rgba(0, 0, 0, 0.6)' }}>Make your trip</div>
+          <div className='text-white mt-[477px] lg:mt-[160px]'>
+            <div className='font-semibold lg:font-bold text-lg mx-auto lg:text-sm mb-[-17px] lg:mb-[-42px]'>Plan your trip with us with the help of AI</div>
+            <div className='fig text-[14vw] lg:text-[150px] font-bold drop-shadow-3xl' style={{ textShadow: '1px 1px 15px rgba(0, 0, 0, 0.6)' }}>Make your trip</div>
           </div>
           <div className='Inputs '>
             <div></div>
-            <div className='flex flex-col gap-3 bg-white px-20 py-10 border rounded-3xl shadow-xl'>
+            <div className='flex flex-col gap-3 bg-white px-3 py-4 lg:px-20 lg:py-10 border rounded-lg  lg:rounded-3xl shadow-xl'>
               <div className='text-sm font-bold'>Please enter your trip details</div>
 
-              <div className='border p-2 rounded-xl flex justify-center items-center'>
-                <input type="text" className='outline-none focus:outline-none w-80 pl-5' placeholder='Enter city' value={city} onChange={(e) => setCity(e.target.value)} />
+              <div className='lg:border w-fit p-2 rounded-xl flex flex-col lg:flex-row justify-center items-center'>
+                <input type="text" className='outline-none border-b lg:border-none focus:outline-none w-80 pl-5' placeholder='Enter city' value={city} onChange={(e) => setCity(e.target.value)} />
 
                 <div className='relative'>
                   <div className='absolute top-[-28px;] right-[41px] text-xs bg-white px-2 font-semibold text-gray-300'>End-date</div>
-                  <input type="date" name="" id="" className='outline-none focus:outline-none border-l px-2' placeholder='Enter city' value={startD} onChange={(e) => setStartD(e.target.value)} />
+                  <input type="date" name="" id="" className='outline-none focus:outline-none lg:border-l lg:px-2' placeholder='Enter city' value={startD} onChange={(e) => setStartD(e.target.value)} />
                 </div>
 
                 <div className='relative'>
                   <div className='absolute top-[-28px;] right-[63px] text-xs bg-white px-2 font-semibold text-gray-300'>End-date</div>
-                  <input type="date" name="" id="" className='outline-none focus:outline-none border-l px-2 border-r mr-5' placeholder='Enter city' value={endD} onChange={(e) => setEndD(e.target.value)} />
+                  <input type="date" name="" id="" className='outline-none focus:outline-none lg:border-l lg:px-2 lg:border-r lg:mr-5' placeholder='Enter city' value={endD} onChange={(e) => setEndD(e.target.value)} />
                 </div>
 
                 <button onClick={handleClick} className='bg-[#41D6C7] p-3 rounded-full'>
